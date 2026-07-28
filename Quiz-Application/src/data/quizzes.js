ef17f65a48be8619094487f1a6847f065e8f3d0d
Tata Anand Kishore <anandkishoretata@gmail.com>
2026-07-28 19:54:49 +0530

    modify code

--- Quiz-Application/src/data/quizzes.js
+++ Quiz-Application/src/data/quizzes.js
@@ -3,29 +3,36 @@ const quizzes = [
     id: "java",
     title: "Java Quiz",
     category: "Programming",
+    difficulty: "Easy",
     questions: 10,
-    time: "10 Min",
+    time: 60,
   },
+
   {
     id: "react",
     title: "React Quiz",
     category: "Frontend",
+    difficulty: "Medium",
     questions: 10,
-    time: "10 Min",
+    time: 90,
   },
+
   {
     id: "mern",
     title: "MERN Quiz",
     category: "Full Stack",
+    difficulty: "Hard",
     questions: 10,
-    time: "10 Min",
+    time: 120,
   },
+
   {
     id: "aptitude",
     title: "Aptitude Quiz",
     category: "Placement",
+    difficulty: "Medium",
     questions: 10,
-    time: "10 Min",
+    time: 90,
   },
 ];
 
